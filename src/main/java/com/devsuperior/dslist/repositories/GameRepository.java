45198e29656973fb5caef5c8ad213411877aa3bd
Vanessa Rutkoski <nessartk@gmail.com>
2025-01-21 15:44:35 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -3,6 +3,6 @@ package com.devsuperior.dslist.repositories;
 import com.devsuperior.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepositoty extends JpaRepository<Game,Long> {
+public interface GameRepository extends JpaRepository<Game,Long> {
 
 }
