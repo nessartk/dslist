580a599a1ad326f264e43ea0fc7a85c3d2ecf646
Vanessa Rutkoski <nessartk@gmail.com>
2025-01-23 15:56:47 -0300

    move games on list

--- src/main/java/com/devsuperior/dslist/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dslist.config;
 
+import com.devsuperior.dslist.projection.GameMinProjection;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig {
@@ -22,5 +25,6 @@ public class WebConfig {
         };
     }
 
-}
+    }
+
 
--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -3,13 +3,11 @@ package com.devsuperior.dslist.controllers;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.dto.ReplacementDTO;
 import com.devsuperior.dslist.service.GameListService;
 import com.devsuperior.dslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,24 +16,31 @@ import java.util.List;
 @RequestMapping(value = "/lists")
 public class GameListController {
 
-        @Autowired
-        private GameListService gameListService;
+    @Autowired
+    private GameListService gameListService;
 
-        @Autowired
-        private GameService gameService;
+    @Autowired
+    private GameService gameService;
 
 
-        @GetMapping
-        public List<GameListDTO> findAll() {
-            List<GameListDTO> result = gameListService.findAll();
-            return result;
-        }
-
-         @GetMapping(value = "/{listId}/games")
-        public List<GameMinDTO> findByList(@PathVariable Long listId) {
-            List<GameMinDTO> result = gameService.findByList(listId);
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> result = gameListService.findAll();
         return result;
     }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId) {
+        List<GameMinDTO> result = gameService.findByList(listId);
+        return result;
     }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId,@RequestBody ReplacementDTO body) {
+       gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+    }
+
+
+}
 
 
